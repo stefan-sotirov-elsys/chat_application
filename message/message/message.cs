67f6555c72487538f3664a31f5a8f81ca31b1f20
YANI <stefan.s.sotirov.2017@elsys-bg.org>
2021-03-23 16:44:24 +0200

    added message

---
+++ message/message/message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace message
+{
+    public class Message
+    {
+        public string type; // the types are : connect, room_create, room_change, send_content, error
+        public string content;
+        public string room_name;
+
+        public Message(string type, string content, string room_name)
+        {
+            this.type = type;
+            this.content = content;
+            this.room_name = room_name;
+        }
+
+        public static byte[] message_to_byte_array(Message message)
+        {
+            BinaryFormatter binary_formatter = new BinaryFormatter();
+            MemoryStream memory_stream = new MemoryStream();
+
+            binary_formatter.Serialize(memory_stream, message);
+            return memory_stream.ToArray();
+        }
+
+        public static Message byte_array_to_message(byte[] bytes)
+        {
+            MemoryStream memory_stream = new MemoryStream();
+            BinaryFormatter binary_formatter = new BinaryFormatter();
+
+            memory_stream.Write(bytes, 0, bytes.Length);
+            memory_stream.Seek(0, SeekOrigin.Begin);
+            Message message = (Message)binary_formatter.Deserialize(memory_stream);
+            return message;
+        }
+    }
+}
